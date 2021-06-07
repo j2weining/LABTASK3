d094b0ec4fc4f9840c57ebe3ae65612b6359b78a
j2weining <weining2001@gmail.com>
2021-06-07 21:28:49 +0800

    lab3

---
+++ function.js
@@ -0,0 +1,8 @@
+function calmessage() {
+    let selection1 = confirm('Are you sure to calculate?');
+            if ( selection1 == true) {
+                
+            } else {
+                alert('Sorry, Please try again.');
+            }
+        }
